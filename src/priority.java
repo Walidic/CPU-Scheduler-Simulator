6eb4b50d2bb821e37f271977369b7831ecb8b87d
Walidic <74151292+Walidic@users.noreply.github.com>
2021-12-13 23:28:06 +0200

    first commit

---
+++ src/priority.java
@@ -0,0 +1,13 @@
+public class priorityScheduling implements IScheduler{
+
+    @Override
+    public void Schedule(Process processes[]) {
+        int count = processes.length;
+        for(int i=0;i<count;i++){
+            for (int j = i;j<count;j++){
+                
+            }
+        }
+    }
+    
+}
