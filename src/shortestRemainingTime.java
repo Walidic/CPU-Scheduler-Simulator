fd82b85a49001d3a432dadddcd95cc4e3e1901eb
Walidic <OMAR.walid123456@gmail.com>
2021-12-28 19:14:12 +0200

    removed strategy pattern

--- src/shortestRemainingTime.java
+++ src/shortestRemainingTime.java
@@ -1,16 +1,16 @@
-public class shortestRemainingTime implements IScheduler{
+public class shortestRemainingTime {
 
-    @Override
     public void Schedule(Process[] processes) {
         processes = reorder(processes, 5);
     }
-    private Process[] reorder(Process[] processes,int count){
-        for(int i=0;i<count;i++){
-            for (int j=i;j<count;j++){
-                if(processes[i].getArrivalTime()>processes[j].getArrivalTime()){
+
+    private Process[] reorder(Process[] processes, int count) {
+        for (int i = 0; i < count; i++) {
+            for (int j = i; j < count; j++) {
+                if (processes[i].getArrivalTime() > processes[j].getArrivalTime()) {
                     Process temp = processes[i];
-                    processes[i]=processes[j];
-                    processes[j]=temp;
+                    processes[i] = processes[j];
+                    processes[j] = temp;
                 }
             }
         }
