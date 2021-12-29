26d14194cdb3b461e3781f8ff44d3b5e9fbe735d
Adham <adhamahmad541@gmail.com>
2021-12-29 15:56:05 +0200

    finished prority schedule

--- src/priority.java
+++ src/priority.java
@@ -1,12 +1,66 @@
 import java.util.Arrays;
-
 public class priority{
+int totalTime;
+int currentTime=0;
 Process procs[];
-    public void waitingTime(Process processes[])
+    public void waitingTime()
     {
         for (int i = 0; i < procs.length; i++)
         {
-            procs[i].setW
+            procs[i].setWaitingTime(procs[i].getExecutionTime()-procs[i].getArrivalTime()); //wait=start-arrival
+        }
+    }
+    public void startTime()
+    {
+        procs[0].setExecutionTime(0);
+        for (int i = 1; i < procs.length; i++)
+        {
+            procs[i].setExecutionTime( procs[i-1].getExecutionTime() + procs[i-1].getBurstTime()); //wait=start-arrival
+        }
+    }
+    public int avgWaitingTime()
+    {   int sum=0;
+        int avg;
+        for (int i = 0; i < procs.length; i++)
+        {
+            sum=sum+procs[i].getWaitingTime();
+        }
+        avg=sum/procs.length;
+        return avg;
+    }
+    public void turnAroundTime()
+    {
+        for (int i = 0; i < procs.length; i++)
+        {
+            procs[i].setTurnAroundTime( procs[i].getWaitingTime() + procs[i].getBurstTime() );
+        }
+    }
+    public int avgTurnAroundTime()
+    {
+        int sum=0;
+        int avg;
+        for (int i = 0; i < procs.length; i++)
+        {
+            sum=sum+procs[i].getTurnAroundTime();
+        }
+        avg=sum/procs.length;
+        return avg;
+    }
+    public void totalTime()
+    {   totalTime=0;
+        for(int i = 0; i < procs.length; i++)
+        {    
+            totalTime= totalTime + procs[i].getBurstTime();
+        }
+    }
+    public void adjustPriority()
+    {
+        for (int i = 0; i < procs.length; i++)
+        {
+            if(procs[i]!=null)
+            {
+                procs[i].setPriorityNumber(procs[i].getPriorityNumber()+1); // inc priority for all processes by 1 as time passes
+            }
         }
     }
     void bubbleSort()
@@ -23,7 +77,7 @@ Process procs[];
                 }
                 else if(procs[j].getArrivalTime() == procs[j+1].getArrivalTime())
                 {
-                    if(procs[j].getPriorityNumber() > procs[j+1].getPriorityNumber())
+                    if(procs[j].getPriorityNumber() > procs[j+1].getPriorityNumber()) //higher priority no = higher priority
                     {
                         // swap arr[j+1] and arr[j]
                         Process temp = procs[j];
@@ -34,7 +88,19 @@ Process procs[];
     }
 
     public Process []Schedule(Process processes[]) {
-        procs=Arrays.copyOf(processes,processes.length); //set local variable so data is shared
+         //set local variable so data is shared
+        procs = new Process[totalTime];
+        int j=0;
+        for (int i = 0; i < processes.length; i++)
+        {
+            int bT = processes[i].getBurstTime();
+            for( j=j; j < j+bT; j++ )
+            {
+                procs[j]=processes[i];
+                currentTime++;
+                adjustPriority();
+            }
+        }
         bubbleSort();
         return procs;
     }
