eaa2f0f4a1a6265fe504685e59c60f9a99a52b12
Walidic <OMAR.walid123456@gmail.com>
2021-12-30 16:29:08 +0200

    stuff

--- src/AGAT.java
+++ src/AGAT.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class AGAT {
     private int v1,v2;
-    
-    public void Schedule(Process[] processes){
-        Process scratch[] = processes;
 
+    public void Schedule(Process[] processes){
+        Process scratch[] = new Process[processes.length];
+        for (int i = 0; i < scratch.length; i++) {
+            scratch[i] = null;
+        }
         int currentTime = 0;
         int totalTime = 0;
         for(int i = 0;i<processes.length;i++){
@@ -16,32 +20,32 @@ public class AGAT {
                 v1 = processes[i].getArrivalTime();
             }
         }
-        int mainArrIndex = 0;
-        while(currentTime<totalTime){
-            if(mainArrIndex == processes.length-1){
-                mainArrIndex = 0;
-            }
+        int scratchIndex = 0;
+        while (currentTime < totalTime) {
             v2 = Integer.MIN_VALUE;
-            for(int i = 0;i<processes.length;i++){
-                if(scratch[i]==null){
-                    continue;
+            for (int i = 0; i < processes.length; i++) {
+                if (v2 < processes[i].getBurstTime()) {
+                    v2 = processes[i].getBurstTime();
                 }
-                if(v2<scratch[i].getBurstTime()){
-                    v2 = scratch[i].getBurstTime();
-                }
-            }
-            int availble[] = new int[scratch.length];
+            } // calculating v2
+
+            int availble[] = new int[processes.length];
             for(int i = 0;i<availble.length;i++){
                 availble[i] = -1;
             }
             for(int i = 0;i<availble.length;i++){
-                if(scratch[i]==null){
-                    continue;
-                }
-                if(scratch[i].getArrivalTime()<=currentTime){
+                if(processes[i].getArrivalTime()<=currentTime){
                     availble[i] = 1;
                 }
-            }
+            } //checking which processes has arrived
+
+            for (int i = 0; i < scratch.length; i++) {
+                if (availble[i] == 1) {
+                    if(scratch[i]==null){
+                        scratch[i]=processes[i];
+                    }
+                }
+            }//filling scratch with arrived proceses only
         }
     }
 }
